cecd0e0877babb0fc7422a5c37cb098568ff552d
JosueDm <sjdominguez@utpl.edu.ec>
2025-07-09 10:45:26 -0500

    Creacion del proyecto

---
+++ sipeip2/src/main/java/snp/sipeip/sipeip2/Sipeip2Application.java
@@ -0,0 +1,13 @@
+package snp.sipeip.sipeip2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sipeip2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sipeip2Application.class, args);
+	}
+
+}
---
+++ sipeip2/src/test/java/snp/sipeip/sipeip2/Sipeip2ApplicationTests.java
@@ -0,0 +1,13 @@
+package snp.sipeip.sipeip2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sipeip2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
